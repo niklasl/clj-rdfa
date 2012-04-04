f61ff5106556e51a2ea3fa5ac77301084e71b3b5
Niklas Lindström <lindstream@gmail.com>
2012-04-04 18:37:12 +0200

    Note about using prefixes in stddom.clj

--- src/rdfa/stddom.clj
+++ src/rdfa/stddom.clj
@@ -87,6 +87,14 @@
     (catch Exception e
       (rdfa.core/error-results (.getMessage e) "en"))))
 
+; TODO: would be more useful if env contained data about *used* prefixes.
+;(defn print-prefixes [{prefix-map :prefix-map vocab :vocab}]
+;  (letfn [(printpfx [pfx iri]
+;            (println (str "@prefix " pfx ": <" iri "> .")))]
+;    (if vocab (printpfx "" vocab))
+;    (doseq [[pfx iri] prefix-map]
+;      (printpfx pfx iri))))
+
 (defn print-triples [triples]
   (doseq [triple triples]
     (-> triple rdfa.repr/repr-triple println)))
@@ -98,6 +106,7 @@
            triples :triples
            proc-triples :proc-triples} (get-rdfa location)]
       (do
+        ;(print-prefixes env)
         (print-triples triples)
         ; TODO: only if --proc in args
         (print-triples proc-triples)))))
