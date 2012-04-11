d55dea8b540cdb2590879d963915fb936802e5d3
Niklas Lindström <lindstream@gmail.com>
2012-04-11 11:32:12 +0200

    Work around bug in java.net.URI for resolving against query string

--- src/rdfa/core.clj
+++ src/rdfa/core.clj
@@ -35,7 +35,13 @@
 
 (defn resolve-iri [iref base]
   (if (not-empty iref)
-    (.. (java.net.URI. base) (resolve iref) (normalize) (toString))
+    ; NOTE: work around bug in java.net.URI for resolving against query string
+    ; See e.g. org.apache.http.client.utils.
+    ;   URIUtils#resolveReferenceStartingWithQueryString
+    (if (.startsWith iref "?")
+      (str (let [i (.indexOf base "?")]
+             (if (> i -1) (subs base 0 i) base)) iref)
+      (.. (java.net.URI. base) (resolve iref) (normalize) (toString)))
     base))
 
 (defn to-iri [s base]
