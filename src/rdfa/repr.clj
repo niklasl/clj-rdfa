882ba87ffba44d50967b6fe91b07ea56c064272d
Mike Travers <miket@quixey.com>
2014-05-14 12:50:44 -0700

    quote strings in a way that makes output legal ntriples readable by jena

--- src/rdfa/repr.clj
+++ src/rdfa/repr.clj
@@ -1,19 +1,24 @@
 (ns rdfa.repr
   (:require rdfa.core))
 
+(defn quote-string [s]
+  (str \" 
+       (clojure.string/replace
+        (clojure.string/replace s #"\""  "\\\\\"")
+        #"\n" "\\\\n")
+       \"))
 
 (defn repr-term [term]
   (condp = (type term)
     rdfa.core.IRI (str "<" (:id term) ">")
-    rdfa.core.Literal (let [{value :value tag :tag} term
-                  qt (if (re-find #"\n|\"" value) "\"\"\"", \")]
-              (str qt value qt
-                   (cond
-                     (= (type tag) rdfa.core.IRI) (str "^^" (repr-term tag))
-                     (not-empty tag) (str "@" tag))))
+    rdfa.core.Literal (let [{value :value tag :tag} term]
+                        (str (quote-string value)
+                             (cond
+                              (= (type tag) rdfa.core.IRI) (str "^^" (repr-term tag))
+                              (not-empty tag) (str "@" tag))))
     rdfa.core.BNode (str "_:" (:id term))
     (throw (IllegalArgumentException.
-             (str "Cannot repr term: " term "(type: " (type term) ")")))))
+            (str "Cannot repr term: " term "(type: " (type term) ")")))))
 
 (defn repr-triple [[s p o]]
   (str (repr-term s) " " (repr-term p) " " (repr-term o) " ."))
